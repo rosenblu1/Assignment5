cd9196d4280f24cb7753371683959b517edf9cb2
rosenblu1 <47091963+rosenblu1@users.noreply.github.com>
2019-02-27 16:03:11 -0600

    Update BlockChain.java

--- BlockChain.java
+++ BlockChain.java
@@ -28,6 +28,7 @@ public class BlockChain {
   public BlockChain(int initial) throws NoSuchAlgorithmException {
     
     MessageDigest md = MessageDigest.getInstance("sha-256");// instantiating the md object
+    //potential hash and potential nonce
     Hash potHash = null;
     long potNonce = 0;
 
@@ -38,6 +39,125 @@ public class BlockChain {
       byte[] nonceBytes = ByteBuffer.allocate(8).putLong(potNonce).array();
 
       md.update(numBytes);
+      md.update(amountBytes);
+      md.update(nonceBytes);
+
+      potHash = new Hash(md.digest());
+      potNonce++;
+    }//while
+
+    this.first.value.hash = potHash;
+    this.first.value.nonce = potNonce - 1;
+    this.first.next = null;
+    this.last = this.first;  
+  }//BlockChain(int amount)
+  
+  public Block mine(int amount) throws NoSuchAlgorithmException {
+    Block next = new Block(this.last.value.getNum() + 1, amount, this.last.value.getHash());
+    return next;
+  }//Block mine(int amount)
+  
+  public int getSize() {
+    return this.last.value.getNum() + 1;
+    }//int getSize()
+  
+  public void append(Block blk) {
+	/*
+	 
+    if ((this.last.value.getNum() != blk.getNum() - 1) || (this.last.value.getHash() != blk.getPrevHash()) || (curAmount <= 0) || (curAmount >= this.first.value.getAmount())) {
+      throw new IllegalArgumentException("Invalid block");
+    }
+    */
+   
+    Node<Block> newNode = new Node<Block>(blk, null);
+    this.last.next = newNode;
+    this.last = newNode;
+    
+    if(!this.isValidBlockChain()) {
+    	this.removeLast();
+    }
+    
+  }//void append(Blockblk)
+  
+  public boolean removeLast() {
+    if (this.first == this.last) {
+      return false;
+    }//if
+    Node<Block> prevBlock = this.first;
+    for (int i = 0;i < this.getSize() - 1;i++) {
+      prevBlock = prevBlock.next;
+    }
+    this.last = prevBlock;
+    this.last.next = null;
+    return true;
+  }
+ 
+  public Hash getHash() {
+    return this.last.value.getHash();
+  }
+ 
+  public boolean isValidBlockChain() {
+   
+	  Node<Block> prev = this.first;
+	  Node<Block> cur = prev.next;
+	//  MessageDigest md = MessageDigest.getInstance("sha-256");
+	  
+	  for(int i = 1; i < this.getSize(); i++) {
+		  
+		  //check blocknums
+		  if (cur.value.getNum() != prev.value.getNum() +1) {
+			  System.out.println("Block numbering inconsistent");
+			  return false;
+		  } 
+		  
+		  //check hashes for validity and consistency
+		  if (!prev.value.getHash().isValid()) {
+			  System.out.println("Hash invalid");
+			  return false;
+		  } else if (cur.value.getPrevHash() != prev.value.getHash()) {
+			  System.out.println("Hashes inconsistent");
+			  return false;
+		  }
+		  
+		  //check amount
+		  int curAmount = cur.value.getAmount() + prev.value.getAmount();
+		  if(curAmount <= 0 || curAmount >= this.first.value.getAmount()) {
+			  System.out.println("Insufficient funds for transaction");
+			  return false;
+		  }
+		  
+		  prev = cur;
+		  cur = cur.next;
+		 
+	  }//for
+	  
+	  return true;
+  }//boolean isValidBlockChain()
+  
+  public void printBalances() {
+    
+    int aliceSum = 0;
+    Node<Block> cur = this.first;
+    
+    for(int i = 1; i < this.getSize(); i++) {
+      aliceSum += cur.value.getAmount();
+      cur = cur.next;
+    }
+    System.out.println("Alice: " + aliceSum + ", Bob: " + (this.first.value.getAmount() - aliceSum));
+  }
+  
+  @Override
+  public String toString() {
+    String str = "";
+    Node<Block> cur = this.first;
+    
+    for(int i = 1; i < this.getSize(); i++) {
+      str = str + cur.value.toString() + '\n';
+      cur = cur.next;
+    }
+    return str;
+  }
+}//BlockChain class
       md.update(amountBytes);
       md.update(nonceBytes);
 
