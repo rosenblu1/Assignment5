230463b494434c8e17b9af5ff2c8218f108d5630
rosenblu1 <47091963+rosenblu1@users.noreply.github.com>
2019-02-27 16:02:03 -0600

    Update BlockChainDriver.java

--- BlockChainDriver.java
+++ BlockChainDriver.java
@@ -1,5 +1,36 @@
 package blockchain;
 
-public class BlockChainDriver {
+import java.util.Scanner;
 
+public class BlockChainDriver {
+	
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			throw new Exception("Incorrect Number of Arguments");
+		} else if (Integer.parseInt(args[0]) < 0) {
+			throw new Exception("Please enter a positive integer");
+		}
+		
+		BlockChain chain = new BlockChain(Integer.parseInt(args[0]));
+		
+		
+	}//main(String[] args)
+	
+	
+	Scanner in = new Scanner(System.in);
+	   boolean done = false;
+	   int i = 0;
+	    while (!done) {
+	      pen.print("Please enter an integer: ");
+	      pen.flush();
+	      
+	      if (!in.hasNextInt()) {
+	        String tmp = in.next();
+	        pen.println("'" + tmp + "' is not an integer.  Try again.");
+	      } else {
+	        i = in.nextInt();
+	        done = true;
+	      } // endif
+	    } // while
 }
+
